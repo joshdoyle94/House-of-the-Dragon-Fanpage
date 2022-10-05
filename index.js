2cec919ab801372a0541c704fd8036bd3ec10617
joshdoyle94 <joshdoyle94@gmail.com>
2022-10-05 09:32:08 -0400

    added new card format to all character cards, started building out js

---
+++ index.js
@@ -0,0 +1,7 @@
+const btn1 = document.getElementById('bio1')
+const btn2 = document.getElementById('bio2')
+const btn3 = document.getElementById('bio3')
+const btn4 = document.getElementById('bio4')
+const btn5 = document.getElementById('bio5')
+const btn6 = document.getElementById('bio6')
+
