64ba62667ab700436256a52ad4826ab624cf4913
joshdoyle94 <joshdoyle94@gmail.com>
2022-10-06 09:43:51 -0400

    added more changes to styling and js

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ const btn3 = document.getElementById('bio3')
 const btn4 = document.getElementById('bio4')
 const btn5 = document.getElementById('bio5')
 const btn6 = document.getElementById('bio6')
+const characterBio1 = document.getElementById('bioInfo1')
+const characterBio2 = document.getElementById('bioInfo2')
+const characterBio3 = document.getElementById('bioInfo3')
+const characterBio4 = document.getElementById('bioInfo4')
+const characterBio5 = document.getElementById('bioInfo5')
+const characterBio6 = document.getElementById('bioInfo6')
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed')
+})
+
+// let btn1Clicked = false
+
+// btn1.addEventListener('click', (event) => {
+//     if (btnClicked === false) {
+//         characterBio1.style.display = 'flex'
+//         btnClicked = false
+//     } else if (bt{
+//         characterBio1.style.display = 'none'
+//     }
+// })
